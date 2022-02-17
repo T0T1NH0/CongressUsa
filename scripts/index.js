e456186af17deba1c1d733ae1f311e00c7d1af96
T0T1NH0 <gaunatoti17@gmail.com>
2022-02-17 07:57:09 -0300

    i added the slidebar in desktop version

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,26 @@
 const ham = document.querySelector('.ham');
-const enlaces = document.querySelector('.linksMenu');
+const enlaces = document.querySelector('.liMenu');
 
 ham.addEventListener('click', () => {
   enlaces.classList.toggle('activado');
   ham.classList.toggle('girar');
 });
+
+const liSubMenu = document.querySelectorAll('.liSubMenu');
+
+for (let i = 0; i < liSubMenu; i++) {
+  liSubMenu[i].addEventListener('click', function () {
+    if (Window.innerWidth < 769) {
+      const subMenu = this.nextElementSibling;
+      const height = subMenu.scrollHeight;
+
+      if (subMenu.classList.contains('desplegar')) {
+        subMenu.classList.remove('desplegar');
+        subMenu.removeAttibute('style');
+      } else {
+        subMenu.classList.add('desplegar');
+        subMenu.style.height = height + 'px';
+      }
+    }
+  });
+}
