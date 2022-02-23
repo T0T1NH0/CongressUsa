69298d59f7b03df745070ab31249c719ad6d0f0e
T0T1NH0 <gaunatoti17@gmail.com>
2022-02-23 17:41:01 -0300

    i finished the layout from the page

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 // HEADER
-
 const ham = document.getElementById('hamburger');
 const enlaces = document.getElementById('liMenu');
 const enlaces2 = document.getElementById('liMenu2');
@@ -12,20 +11,38 @@ ham.addEventListener('click', () => {
   ham.classList.toggle('girar');
 });
 
+const liSubMenu1 = document.querySelector('.liSubMenu');
+const liSubMenu2 = document.querySelector('.liSubMenu2');
+const liSubMenu3 = document.querySelector('.liSubMenu3');
+const ulMenu1 = document.querySelector('.clickMenu');
+const ulMenu2 = document.querySelector('.clickMenu2');
+const ulMenu3 = document.querySelector('.clickMenu3');
+
+liSubMenu1.addEventListener('click', () => {
+  ulMenu1.classList.toggle('activado');
+});
+liSubMenu2.addEventListener('click', () => {
+  ulMenu2.classList.toggle('activado');
+});
+liSubMenu3.addEventListener('click', () => {
+  ulMenu3.classList.toggle('activado');
+});
+
 // READ MORE / READ LESS BUTTON
 
 let hideTextBtn = document.getElementById('hideTextBtn');
 let hideText = document.getElementById('hideText');
 
 const toggleText = () => {
-  hideText.classList.toggle('show');
+  hideText.classList.toggle('showReadLess');
 
-  hideText.classList.contains('show')
+  hideText.classList.contains('showReadLess')
     ? (hideTextBtn.innerHTML = 'Read Less')
     : (hideTextBtn.innerHTML = 'Read More');
 };
-
-hideTextBtn.addEventListener('click', () => toggleText());
+try {
+  hideTextBtn.addEventListener('click', () => toggleText());
+} catch (error) {}
 
 // GO TOP
 
