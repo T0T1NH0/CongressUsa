2b6f672e23d178086c02739e54c2e12722f3bb69
T0T1NH0 <gaunatoti17@gmail.com>
2022-03-03 01:16:08 -0300

    Data put in table and scroll X and Y added.

--- scripts/Modules/Congress113/congressHouse113.js
+++ scripts/Modules/Congress113/congressHouse113.js
@@ -1,6 +1,6 @@
 import { houseData } from '../Services/dataHouse.js';
 let csh = houseData.results[0].members;
-
+console.log(csh);
 const getState = () => {
   let allState = [];
   csh.forEach((member) => {
@@ -17,4 +17,35 @@ const getState = () => {
     selector.appendChild(option);
   });
 };
+
+const tdInfo = (info) => {
+  let td = document.createElement('td');
+  td.innerText = info;
+  return td;
+};
+
+const getTableMembers = () => {
+  let tableBody = document.getElementById('tBody');
+  csh.map((members) => {
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    let memberUrl = document.createElement('a');
+
+    memberUrl.innerText = `${members.last_name} ${
+      members.middle_name !== null ? members.middle_name : ''
+    } ${members.first_name}`;
+    memberUrl.href = members.url;
+    memberUrl.target = '_blank';
+    td.append(memberUrl);
+
+    tr.append(td);
+    tr.append(tdInfo(members.party));
+    tr.append(tdInfo(members.state));
+    tr.append(tdInfo(members.seniority));
+    tr.append(tdInfo(members.votes_with_party_pct));
+
+    tableBody.append(tr);
+  });
+};
 getState();
+getTableMembers();
--- scripts/Modules/Congress113/congressSenate113.js
+++ scripts/Modules/Congress113/congressSenate113.js
@@ -1,10 +1,10 @@
 import { senateData } from '../Services/dataSenate.js';
 
-let csS = senateData.results[0].members;
+let cSS = senateData.results[0].members;
 
 const getState = () => {
   let allState = [];
-  csS.forEach((member) => {
+  cSS.forEach((member) => {
     if (!allState.includes(member.state)) {
       allState.push(member.state);
     }
@@ -18,4 +18,34 @@ const getState = () => {
     selector.appendChild(option);
   });
 };
+const tdInfo = (info) => {
+  let td = document.createElement('td');
+  td.innerText = info;
+  return td;
+};
+
+const getTableMembers = () => {
+  let tableBody = document.getElementById('tBody');
+  cSS.map((members) => {
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    let memberUrl = document.createElement('a');
+
+    memberUrl.innerText = `${members.last_name} ${
+      members.middle_name !== null ? members.middle_name : ''
+    } ${members.first_name}`;
+    memberUrl.href = members.url;
+    memberUrl.target = '_blank';
+    td.append(memberUrl);
+
+    tr.append(td);
+    tr.append(tdInfo(members.party));
+    tr.append(tdInfo(members.state));
+    tr.append(tdInfo(members.seniority));
+    tr.append(tdInfo(members.votes_with_party_pct));
+
+    tableBody.append(tr);
+  });
+};
 getState();
+getTableMembers();
